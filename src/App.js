a27adcb6349f8543e0cae7e65a9610fe6517f7ac
Rita <96919690+RitaCr23@users.noreply.github.com>
2022-05-26 10:27:12 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Questions from "./components/Questions";
+
+export default function App() {
+  return (
+    <div>
+      <Questions />
+    </div>
+  );
+}
+
+/*<h1>Quizz</h1>
+      <button className="start-btn">Start Quizz</button>*/
