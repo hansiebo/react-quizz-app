42d4e9fe7995541e1479bc4a1d6d6af696e6c481
Rita <96919690+RitaCr23@users.noreply.github.com>
2022-09-08 23:40:42 +0100

    Add files via upload

---
+++ src/components/Answers.js
@@ -0,0 +1,7 @@
+// import React from "react";
+
+// export default function Answers(props){
+//     const styles = {
+//         backgroundColor: props.isHeld ? ""
+//     }
+// }
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+
+// Requirements:
+// two screens to start and quizz
+// Tally correct answers after "check answers is clicked"
 
 export default function Quizz(props) {
+  //call API once button is clicked and display questions
+  const [startQuizz, setStartQuizz] = useState(false);
+  //set the questions
   const [questions, setQuestions] = useState([]);
+  //indicator if game was won
+  const [quizzical, setQuizzical] = useState(false);
 
+  //API call request
   useEffect(() => {
     fetch(
       "https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple"
@@ -12,29 +23,29 @@ export default function Quizz(props) {
       .then((data) => setQuestions(data.results));
   }, []);
 
-  //CREATE ELEMENTS TO DISPLAY ON PAGE
+  //Variable that stores the API elements that will be displayed on the page
   const questionElements = questions.map((item) => {
-    //MERGE ANSWERS AND SHUFFLE THEM
+    //Merge all answer options and shuffle them to give a random position for correct and incorrect answers
     const allAnswers = [item.correct_answer, ...item.incorrect_answers].sort(
       () => Math.random() - 0.5
     );
-
-    //FUNCTION TO CHECK ANSWERS
-    //CHANGE BUTTON COLOUR BASED ON CORRECT OR INCORRECT ANSWERS
-    //DISABLE BUTTON COLOUS AFTER THEY'RE SELECTED
-    //FUNCTION TO ENABLE CHECK ANSWERS BUTTON
-    //COUNT HOW MANY CORRECT ANSWERS
-    //RENDER PLAY AGAIN BUTTON
-
     return (
       <div className="questions-card">
-        <h2 className="questions" key={Math.random(Math.floor() * 5)}>
+        <h2 className="questions" key={nanoid()}>
           {item.question}
         </h2>
-        <button className="answer-btn">{allAnswers[0]}</button>
-        <button className="answer-btn">{allAnswers[1]}</button>
-        <button className="answer-btn">{allAnswers[2]}</button>
-        <button className="answer-btn">{allAnswers[3]}</button>
+        <button className="answer-btn" key={nanoid()}>
+          {allAnswers[0]}
+        </button>
+        <button className="answer-btn" key={nanoid()}>
+          {allAnswers[1]}
+        </button>
+        <button className="answer-btn" key={nanoid()}>
+          {allAnswers[2]}
+        </button>
+        <button className="answer-btn" key={nanoid()}>
+          {allAnswers[3]}
+        </button>
         <hr></hr>
       </div>
     );
@@ -47,3 +58,29 @@ export default function Quizz(props) {
     </div>
   );
 }
+
+//FUNCTION TO CHECK ANSWERS
+//CHANGE BUTTON COLOUR BASED ON CORRECT OR INCORRECT ANSWERS
+//DISABLE BUTTON COLOUS AFTER THEY'RE SELECTED
+//FUNCTION TO ENABLE CHECK ANSWERS BUTTON
+//COUNT HOW MANY CORRECT ANSWERS
+//RENDER PLAY AGAIN BUTTON
+
+/*<div>
+
+//DISPLAY DATA OBJECT FROM STRING
+{JSON.stringify(questions)}
+
+
+      //STYLES
+      <button className="options">Option</button>
+      <button className="correct_answer">Right</button>
+      <button className="wrong_answer">Wrong</button>
+      <button className="options">Option</button>
+      <hr></hr>
+      
+<button>{item.correct_answer}</button>
+      <button>{item.incorrect_answers[0]}</button>
+      <button>{item.incorrect_answers[1]}</button>
+      <button>{item.incorrect_answers[2]}</button>
+    </div>*/
